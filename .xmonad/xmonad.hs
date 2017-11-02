42b079d9ce75b7cce89311c2c1a7973eff4bd0b1
clockvoid <hidenari_1@yahoo.co.jp>
2017-11-02 22:19:18 +0900

    fix: indentation for xmonad.hs and .xmobarrc

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -13,25 +13,27 @@ baseConfig = desktopConfig
 
 -- settings of defalut workspace for some applications
 myManageShift = composeAll
-    [ className =? "Mikutter.rb" --> doShift "9"
-    ]
+  [ className =? "Mikutter.rb" --> doShift "9"
+  ]
 
 -- settings of Window management
 myManageHook = composeAll
-    [ className =? "Gimp" --> doFloat
-    , className =? "Vncviewer" --> doFloat
-    , className =? "Caja" --> doCenterFloat
-    , className =? "Pcmanfm" --> doCenterFloat
-    , className =? "Engrampa" --> doCenterFloat
-    , className =? "Eom" --> doCenterFloat
-    , className =? "Vidalia" --> doCenterFloat
-    , className =? "Mikutter.rb" <&&> title =? "Preferences" --> doCenterFloat
-    , appName =? "crx_nckgahadagoaajjgafhacjanaoiihapd" --> doCenterFloat
-    , className =? "Gnome-system-monitor" --> doCenterFloat
-    , isFullscreen --> doFloat
-    , isDialog --> doCenterFloat
-    , title =? "Nice Window" --> doCenterFloat
-    ]
+  [ className =? "Gimp" --> doFloat
+  , className =? "Vncviewer" --> doFloat
+  , className =? "Caja" --> doCenterFloat
+  , className =? "Pcmanfm" --> doCenterFloat
+  , className =? "Engrampa" --> doCenterFloat
+  , className =? "Eom" --> doCenterFloat
+  , className =? "Vidalia" --> doCenterFloat
+  , className =? "Mikutter.rb" <&&> title =? "Preferences" --> doCenterFloat
+  , appName =? "crx_nckgahadagoaajjgafhacjanaoiihapd" --> doCenterFloat
+  , className =? "Gnome-system-monitor" --> doCenterFloat
+  , isFullscreen --> doFloat
+  , isDialog --> doCenterFloat
+  , title =? "Nice Window" --> doCenterFloat
+  , title =? "Cell Automaton" --> doCenterFloat
+  , className =? "feh" --> doCenterFloat
+  ]
 
 -- settings for new shortcut keys
 myKeys = [ ("M-p", spawn "dmenu_run -fn 'Monospace-11'")
@@ -59,33 +61,35 @@ myNomalBorderColor = "#1C1C1C"
 
 -- main function
 main = do
-    {-
-     - xmproc: constant that is xmobar spawnPipe
-     - xmonad function is a Monad takes Default as an argument.
-     - Ofcorse, fullscreenSupport is Default that takes Default and is enable fullscreen application like Google Chrome work well.
-     - baseConfig is XConfig and it defines as desktopConfig.
-     - desktopConfig is XConfig that configures xmonad to ignore and leave room for dock type windows like panels and trays, adds the default key binding to toggle panel visibility, and activates basic EWMH supprt.
-     - XConfig is constructor of data XConfig l.
-     - XConfig has some field value. there are details in http://xmonad.org/xmonad-docs/xmonad/XMonad-Core.html#t:XConfig.
-     - additionalKeysP :: XConfig l -> [(String, X ())] -> XConfig l
-     - additionalKeysP takes XConfig and touple list and output edited XConfig.
-     - additionalKeysP can add some key bindings.
-     -}
-    xmproc <- spawnPipe "/usr/bin/xmobar"
-    xmonad $ fullscreenSupport $ baseConfig
-        { terminal  = myTerminal
-        , borderWidth = myBorderWidth
-        , focusedBorderColor = myForcusedBorderColor
-        , normalBorderColor = myNomalBorderColor
-        , manageHook = manageDocks <+> myManageShift <+> myManageHook <+> manageHook baseConfig
-        , layoutHook = avoidStruts $ layoutHook baseConfig
-        --, modMask = myModMask
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
-
-        }
-
-        `additionalKeysP` myKeys
-        
+  {-
+   - xmproc: constant that is xmobar spawnPipe
+   - xmonad function is a Monad takes Default as an argument.
+   - Ofcorse, fullscreenSupport is Default that takes Default and is enable fullscreen application like Google Chrome work well.
+   - baseConfig is XConfig and it defines as desktopConfig.
+   - desktopConfig is XConfig that configures xmonad to ignore and leave room for dock type windows like panels and trays, adds the default key binding to toggle panel visibility, and activates basic EWMH supprt.
+   - XConfig is constructor of data XConfig l.
+   - XConfig has some field value. there are details in http://xmonad.org/xmonad-docs/xmonad/XMonad-Core.html#t:XConfig.
+   - additionalKeysP :: XConfig l -> [(String, X ())] -> XConfig l
+   - additionalKeysP takes XConfig and touple list and output edited XConfig.
+   - additionalKeysP can add some key bindings.
+   -}
+  xmproc <- spawnPipe "/usr/bin/xmobar"
+  xmonad $ fullscreenSupport $ baseConfig
+    { terminal  = myTerminal
+    , borderWidth = myBorderWidth
+    , focusedBorderColor = myForcusedBorderColor
+    , normalBorderColor = myNomalBorderColor
+    , manageHook = manageDocks <+> myManageShift <+> myManageHook <+> manageHook baseConfig
+    , layoutHook = avoidStruts $ layoutHook baseConfig
+    --, modMask = myModMask
+    , logHook = dynamicLogWithPP xmobarPP
+                    { ppOutput = hPutStrLn xmproc
+                    , ppTitle = xmobarColor "green" "" . shorten 50 . \s -> "Main: " ++ s
+                    , ppLayout = \s -> "<" ++ s ++ ">"
+                    , ppSep = " | "
+                    }
+    
+    }
+    
+    `additionalKeysP` myKeys
+    
