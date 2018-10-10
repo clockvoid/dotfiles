f76e67a466a8c325c7b0cf006b15d23b5fb2865c
clockvoid <hidenari.t1997nov@gmail.com>
2018-10-10 10:58:29 +0900

    fix: xmoand.hs
    terminal change to terminator

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -50,11 +50,12 @@ myKeys = [ ("M-p", spawn "dmenu_run -fn 'monospace-11'")
          , ("M-n", spawn "nvim-wrapper")
          , ("M-g", spawn "gvim")
          , ("C-S-<Esc>", spawn "gnome-system-monitor")
-         , ("M-x", spawn "xterm")
+         , ("M-x", spawn "terminator")
          ] 
 
 -- settings for default terminal emulator
-myTerminal = "xterm -e zsh -c \"tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER@$HOSTNAME\""
+-- myTerminal = "xterm -e zsh -c \"tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER@$HOSTNAME\""
+myTerminal = "terminator -x zsh -c \"tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER@$HOSTNAME\""
 
 -- settings for mod key
 myModMask = mod4Mask
