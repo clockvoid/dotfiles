19617e2a940897bfc0417504f481c5224e826e92
clockvoid <hidenari.t1997nov@gmail.com>
2020-10-08 00:00:59 +0900

    add: path to llvm path

--- Linux/.config/zsh/env/path.zsh
+++ Linux/.config/zsh/env/path.zsh
@@ -1,7 +1,8 @@
 # settings for path
 export ANYENV_ROOT=$HOME/.anyenv
 export CUDA_ROOT=/usr/local/cuda
-export PATH=$CUDA_ROOT/bin:$ANYENV_ROOT/bin:$PATH:$HOME/.gem/ruby/2.4.0/bin:$HOME/.local/bin:$HOME/.cargo/bin
+export LLVM_ROOT=/usr/lib/llvm-7
+export PATH=$LLVM_ROOT/bin:$CUDA_ROOT/bin:$ANYENV_ROOT/bin:$PATH:$HOME/.gem/ruby/2.4.0/bin:$HOME/.local/bin:$HOME/.cargo/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:CUDA_ROOT/lib64
 
 eval "$(anyenv init -)"
