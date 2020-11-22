5dc30f70d8d0155c74b457d43bfab51a3e90b707
clockvoid <hidenari.t1997nov@gmail.com>
2020-11-22 16:10:22 +0900

    add: fzf setting

---
+++ Common/.config/zsh/.fzf.zsh
@@ -0,0 +1,33 @@
+# Setup fzf
+# ---------
+
+if ! type fzf > /dev/null; then
+
+    echo "Please install fzf!"
+else
+
+    if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+      export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+    fi
+
+    if ! type bat > /dev/null; then
+        echo "Please install bat."
+    else
+        export FZF_DEFAULT_OPTS='--preview "bat  --color=always --style=header,grid --line-range :100 {}" --height 40% --border'
+    fi
+
+    if ! type rg > /dev/null; then
+        echo "Plase install ripgrep."
+    else
+        export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
+    fi
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+fi
+
