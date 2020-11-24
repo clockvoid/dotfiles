4a704672f9f44912eba939afd694252047668800
clockvoid <hidenari.t1997nov@gmail.com>
2020-11-24 23:38:39 +0900

    fix: fzf setting

--- Common/.config/zsh/init/fzf.zsh
+++ Common/.config/zsh/init/fzf.zsh
@@ -1,3 +1,33 @@
-# for fzf searching hidden files
-export FZF_DEFAULT_COMMAND="find . -path ./dist -prune -o -path ./.git -prune -o -type f -print -o -type l -print 2> /dev/null | sed s/^..//"
+# Setup fzf
+# ---------
+
+if ! type fzf > /dev/null; then
+
+    echo "Please install fzf!"
+else
+
+    if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+      export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+    fi
+
+    if ! type bat > /dev/null; then
+        echo "Please install bat."
+    else
+        export FZF_DEFAULT_OPTS='--reverse --preview "bat  --color=always --style=header,grid --line-range :100 {}" --height 40% --border'
+    fi
+
+    if ! type rg > /dev/null; then
+        echo "Plase install ripgrep."
+    else
+        export FZF_CTRL_T_COMMAND='rg --files --hidden --follow --glob "\!.git/**" --glob "\!Library/**"'
+    fi
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+fi
 
