ab150d3333ef527126f73939b94c0efbaa840f7f
clockvoid <hidenari.t1997nov@gmail.com>
2020-11-25 22:55:44 +0900

    fix: use fd in fzf

--- Common/.config/zsh/init/fzf.zsh
+++ Common/.config/zsh/init/fzf.zsh
@@ -16,11 +16,39 @@ else
         export FZF_DEFAULT_OPTS='--reverse --preview "bat  --color=always --style=header,grid --line-range :100 {}" --height 40% --border'
     fi
 
-    if ! type rg > /dev/null; then
-        echo "Plase install ripgrep."
+    if ! type fd > /dev/null; then
+        echo "Plase install fd."
     else
-        export FZF_CTRL_T_COMMAND='rg --files --hidden --follow --glob "\!.git/**" --glob "\!Library/**"'
+        export FZF_CTRL_T_COMMAND='fd --hidden --follow --exclude ".git" --exclude "Library" --max-depth 5'
     fi
+
+    # Use fd (https://github.com/sharkdp/fd) instead of the default find
+    # command for listing path candidates.
+    # - The first argument to the function ($1) is the base path to start traversal
+    # - See the source code (completion.{bash,zsh}) for the details.
+    _fzf_compgen_path() {
+      fd --hidden --follow --exclude ".git" . "$1"
+    }
+    
+    # Use fd to generate the list for directory completion
+    _fzf_compgen_dir() {
+      fd --type d --hidden --follow --exclude ".git" . "$1"
+    }
+    
+    # (EXPERIMENTAL) Advanced customization of fzf options via _fzf_comprun function
+    # - The first argument to the function is the name of the command.
+    # - You should make sure to pass the rest of the arguments to fzf.
+    _fzf_comprun() {
+      local command=$1
+      shift
+    
+      case "$command" in
+        cd)           fzf "$@" --preview 'tree -C {} | head -200' ;;
+        export|unset) fzf "$@" --preview "eval 'echo \$'{}" ;;
+        ssh)          fzf "$@" --preview 'dig {}' ;;
+        *)            fzf "$@" ;;
+      esac
+    }
 
     # Auto-completion
     # ---------------
