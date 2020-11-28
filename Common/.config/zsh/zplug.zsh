abb858e97485cd456f445ac6a176343be8cc877d
clockvoid <hidenari.t1997nov@gmail.com>
2020-11-28 22:36:16 +0900

    add: forgit to zplug

--- Common/.config/zsh/zplug.zsh
+++ Common/.config/zsh/zplug.zsh
@@ -6,6 +6,7 @@ source $ZPLUG_HOME/init.zsh
 zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(vi_mode dir vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
+zplug 'wfxr/forgit'
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
