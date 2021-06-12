3d203be3e87a839057f6e1392ec27074bba432b8
clockvoid <hidenari.t1997nov@gmail.com>
2021-06-12 21:34:33 +0900

    add: gopath to path.zsh for mac

--- Darwin/.config/zsh/env/path.zsh
+++ Darwin/.config/zsh/env/path.zsh
@@ -5,3 +5,5 @@ export ANDROID_SDK_HOME=$HOME/Library/Android/sdk
 export PATH=/usr/local/opt/coreutils/libexec/gnubin:$JAVA_HOME/bin:$ANDROID_SDK_HOME/platform-tools:$ANYENV_ROOT/bin:$PATH:$HOME/.local/bin:$HOME/.cargo/bin
 
 eval "$(anyenv init -)"
+
+export PATH=$GOROOT/bin:$PATH
