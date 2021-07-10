4009b1fbd66eb8846497d9cb3f4007de0f841ab3
clockvoid <hidenari.t1997nov@gmail.com>
2021-07-10 23:30:52 +0900

    fix: keychain setting for linux: disable on tmux

--- Linux/.config/zsh/env/keychain.zsh
+++ Linux/.config/zsh/env/keychain.zsh
@@ -1,5 +1,7 @@
 if ! type "keychain" > /dev/null; then
     echo 'Please isntall keychain using package manager!'
 else
-    eval $(keychain --eval --agents ssh -Q --quiet ~/.ssh/id_rsa)
+    if ! { [ -n "$TMUX" ]; } then
+        eval $(keychain --eval --agents ssh -Q --quiet ~/.ssh/id_*)
+    fi
 fi
