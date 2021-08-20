e75c076ec35a57b27616e788155ee97cf3f06492
clockvoid <hidenari.t1997nov@gmail.com>
2021-08-21 00:34:06 +0900

    fix: selected screenshot background to transparent

--- Linux/.xmonad/screenshot_select.sh
+++ Linux/.xmonad/screenshot_select.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #maim -s $1/Screenshot_at_$(date +%Y-%m-%d-%H-%M-%S).png
-maim -s | convert - \( +clone -background black -shadow 60x30+0+10 \) +swap -background white -layers merge +repage $1/Screenshot_at_$(date +%Y-%m-%d-%H-%M-%S).png
+maim -s | convert - \( +clone -background black -shadow 60x30+0+10 \) +swap -background none -layers merge +repage $1/Screenshot_at_$(date +%Y-%m-%d-%H-%M-%S).png
