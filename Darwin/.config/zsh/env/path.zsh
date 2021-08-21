c686e6f91c857434677d57dde0988ea6795c3812
clockvoid <hidenari.t1997nov@gmail.com>
2021-08-22 04:38:07 +0900

    add: flutter to path for mac

--- Darwin/.config/zsh/env/path.zsh
+++ Darwin/.config/zsh/env/path.zsh
@@ -2,7 +2,7 @@
 export ANYENV_ROOT=$HOME/.anyenv
 export JAVA_HOME=`/usr/libexec/java_home`
 export ANDROID_SDK_HOME=$HOME/Library/Android/sdk
-export PATH=/usr/local/opt/coreutils/libexec/gnubin:$JAVA_HOME/bin:$ANDROID_SDK_HOME/platform-tools:$ANYENV_ROOT/bin:$PATH:$HOME/.local/bin:$HOME/.cargo/bin
+export PATH=/usr/local/opt/coreutils/libexec/gnubin:$JAVA_HOME/bin:$ANDROID_SDK_HOME/platform-tools:$ANYENV_ROOT/bin:$PATH:$HOME/.local/bin:$HOME/.cargo/bin:$HOME/git/flutter/flutter/bin
 
 eval "$(anyenv init -)"
 
