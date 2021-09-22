68c5854b0f4bc64c69bd160eee7daef97d63a188
clockvoid <hidenari.t1997nov@gmail.com>
2021-09-23 01:58:17 +0900

    fix: screenshot background shadow setting

--- Linux/.xmonad/screenshot_focused_window.sh
+++ Linux/.xmonad/screenshot_focused_window.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-maim -i $(xdotool getwindowfocus) $1/Screenshot_at_$(date +%Y-%m-%d-%H-%M-%S).png
+maim -i $(xdotool getwindowfocus) | convert - \( +clone -background black -shadow 60x30+0+10 \) +swap -background none -layers merge +repage $1/Screenshot_at_$(date +%Y-%m-%d-%H-%M-%S).png
