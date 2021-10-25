7e9766185d1ce2752a2785c248b957a7de20dfdd
clockvoid <hidenari.t1997nov@gmail.com>
2021-10-25 19:47:01 +0900

    add: apple keyboard conf to set_keyboard script

--- Linux/.xmonad/set_keyboard.sh
+++ Linux/.xmonad/set_keyboard.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-letsnote=$(xinput list --id-only 'AT Translated Set 2 keyboard')
+pcnote=$(xinput list --id-only 'AT Translated Set 2 keyboard')
 
 if [ $? -eq 0 ]; then
-    setxkbmap -device $letsnote -layout jp -option "ctrl:nocaps"
+    setxkbmap -device $pcnote -layout jp -option "ctrl:nocaps"
+fi
+
+apple=$(xinput list --id-only 'Apple Inc. Apple Internal Keyboard / Trackpad')
+
+if [ $? -eq 0 ]; then
+    setxkbmap -device $apple -layout jp -option "ctrl:nocaps"
 fi
 
 realforce=$(xinput list --id-only 'Topre REALFORCE 87 US')
