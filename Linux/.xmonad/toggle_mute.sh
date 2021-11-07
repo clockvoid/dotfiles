ba67fb821c2a9bdf94d037c6dee86dbea00372d3
clockvoid <hidenari.t1997nov@gmail.com>
2021-11-08 05:05:11 +0900

    delete: play sample sound when volume is changed

--- Linux/.xmonad/toggle_mute.sh
+++ Linux/.xmonad/toggle_mute.sh
@@ -2,5 +2,3 @@
 
 sink=`pactl get-default-sink`
 pactl set-sink-mute $sink toggle
-sleep 0.05
-pactl play-sample audio-volume-change $sink
--- Linux/.xmonad/volume_down.sh
+++ Linux/.xmonad/volume_down.sh
@@ -2,5 +2,3 @@
 
 sink=`pactl get-default-sink`
 pactl set-sink-volume $sink -5%
-sleep 0.05
-pactl play-sample audio-volume-change $sink
