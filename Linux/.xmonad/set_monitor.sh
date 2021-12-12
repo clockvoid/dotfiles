ce6698cddca626082409838f823c6008d69282f4
clockvoid <hidenari.t1997nov@gmail.com>
2021-12-12 20:26:08 +0900

    fix: xmonad monitor setting when restart xmonad

--- Linux/.xmonad/set_monitor.sh
+++ Linux/.xmonad/set_monitor.sh
@@ -7,18 +7,30 @@ fi
 
 INTERNAL="eDP1"
 IS_CONNECT_EXTERNAL="false"
+EXTERNAL_PRIMARY="HDMI1"
+
+xrandr --output "$INTERNAL" --primary --auto
 
 old_IFS="${IFS}"
 IFS=$'\n'
 for out in `xrandr | grep connected | cut -d ' ' -f 1-2`; do
     name=`echo $out | cut -d ' ' -f 1`
     state=`echo $out | cut -d ' ' -f 2`
-    echo "name is $name, INTERNAL is ${INTERNAL}"
-    if [ "$state" = "connected" ]; then
-        if [ ! "$name" = ${INTERNAL} ]; then
-            IS_CONNECT_EXTERNAL="true"
-        fi
-    fi
+    case "$state" in
+        "connected"*)
+            if [ "$name" = "${EXTERNAL_PRIMARY}" ]; then
+                IS_CONNECT_EXTERNAL="true"
+                xrandr --output "$EXTERNAL_PRIMARY" --primary --auto
+            elif [ "$name" != "${INTERNAL}" ]; then
+                xrandr --output "$name" --auto --right-of "${EXTERNAL_PRIMARY}"
+            fi
+            ;;
+        "disconnected"*)
+                xrandr --output "$name" --off
+            ;;
+        *)
+            ;;
+    esac
 done
 IFS="${old_IFS}"
 
@@ -26,10 +38,15 @@ if [ "$IS_CONNECT_EXTERNAL" = "false" ]; then
     if [ `pidof redshift | wc -w` = 0 ]; then
         redshift -P -m randr -t 5700:3600 -b 1.0:1.0 -l 35.465:139.622 &
     fi
-    xrandr --output eDP1 --primary --auto
 else
     for pid in `pidof redshift`; do
         redshift -xP
         kill $pid
     done
+    lid_state=`cat /proc/acpi/button/lid/LID0/state | sed 's/ \+/ /g' | cut -d' ' -f2`
+    if [ $lid_state = "closed" ]; then
+        xrandr --output "$INTERNAL" --off
+    else
+        xrandr --output "$INTERNAL" --auto --pos 320x1440
+    fi
 fi
