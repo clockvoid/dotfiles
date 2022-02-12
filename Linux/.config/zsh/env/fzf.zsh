9116c414376e04a8a6de89a18bac58d2ed443511
clockvoid <hidenari.t1997nov@gmail.com>
2022-02-12 15:21:30 +0900

    add: fzf setting file for linux

---
+++ Linux/.config/zsh/env/fzf.zsh
@@ -0,0 +1,20 @@
+export FZF_DEFAULT_COMMAND='find \( -type d \( \
+           -name .git \
+        \) -prune \) -o -type f -printf "%P\n"'
+
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+    find "$1" \( -type d \( \
+           -name .git \
+        \) -prune \) -o -type f -printf "%P\n"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+    find "$1" \( -type d \( \
+           -name .git \
+        \) -prune \) -o -type d -printf "%P\n"
+}
