ed4b6ead005d884c0b4de08a027a448fb022c335
Lucciola <clockvoid@users.noreply.github.com>
2022-02-12 23:20:16 +0900

    add: gnu find utils for mac

--- Darwin/.config/zsh/env/path.zsh
+++ Darwin/.config/zsh/env/path.zsh
@@ -1,4 +1,4 @@
 # settings for path
 export JAVA_HOME=`/usr/libexec/java_home`
 export ANDROID_SDK_HOME=$HOME/Library/Android/sdk
-export PATH=$GOROOT/bin:/usr/local/opt/coreutils/libexec/gnubin:$JAVA_HOME/bin:$ANDROID_SDK_HOME/platform-tools:$HOME/git/flutter/flutter/bin:$PATH
+export PATH=/usr/local/opt/findutils/libexec/gnubin:$GOROOT/bin:/usr/local/opt/coreutils/libexec/gnubin:$JAVA_HOME/bin:$ANDROID_SDK_HOME/platform-tools:$HOME/git/flutter/flutter/bin:$PATH
