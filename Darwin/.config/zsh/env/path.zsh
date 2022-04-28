38e3cbf1d68cda3e5d29f6f584c659af0598d162
Lucciola <clockvoid@users.noreply.github.com>
2022-04-29 01:42:19 +0900

    fix: path environment valuable for macos

--- Darwin/.config/zsh/env/path.zsh
+++ Darwin/.config/zsh/env/path.zsh
@@ -1,4 +1,8 @@
 # settings for path
 export JAVA_HOME=`/usr/libexec/java_home`
 export ANDROID_SDK_HOME=$HOME/Library/Android/sdk
-export PATH=/usr/local/opt/findutils/libexec/gnubin:$GOROOT/bin:/usr/local/opt/coreutils/libexec/gnubin:$JAVA_HOME/bin:$ANDROID_SDK_HOME/platform-tools:$HOME/git/flutter/flutter/bin:$PATH
+export FLUTTER_HOME=$HOME/git/flutter/flutter
+export FIND_UTILS=$(brew --prefix)/opt/findutils/libexec/gnubin
+export CORE_UTILS=$(brew --prefix)/opt/coreutils/libexec/gnubin
+
+export PATH=$FIND_UTILS:$CORE_UTILS:$GOROOT/bin:$JAVA_HOME/bin:$ANDROID_SDK_HOME/platform-tools:$FLUTTER_HOME/bin:$PATH
