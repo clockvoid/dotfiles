aefccd8ea8c368ba03d49373245467f05bec0c04
clockvoid <hidenari.t1997nov@gmail.com>
2022-08-29 00:20:30 +0900

    fix: linux path setting

--- Linux/.config/zsh/env/path.zsh
+++ Linux/.config/zsh/env/path.zsh
@@ -1,6 +1,8 @@
 # settings for path
 export CUDA_ROOT=/usr/local/cuda
 export LLVM_ROOT=/usr/lib/llvm-7
-export PATH=$LLVM_ROOT/bin:$CUDA_ROOT/bin:$HOME/.gem/ruby/2.4.0/bin:$HOME/Android/Sdk/platform-tools:$HOME/Android/Sdk/cmdline-tools/latest/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:CUDA_ROOT/lib64
 export CHROME_EXECUTABLE=chromium
+export GOPATH=$(go env GOPATH)
+
+export PATH=$LLVM_ROOT/bin:$CUDA_ROOT/bin:$HOME/.gem/ruby/2.4.0/bin:$HOME/Android/Sdk/platform-tools:$HOME/Android/Sdk/cmdline-tools/latest/bin:$GOPATH/bin:$PATH
