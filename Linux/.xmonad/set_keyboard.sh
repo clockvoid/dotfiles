2176453a62dfcd6dfdcd2e7fb08561c7c886817d
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2023-01-03 00:33:41 +0900

    fix: xmonad keyboard and monitor setting for thinkpad x1 nano

--- Linux/.xmonad/set_keyboard.sh
+++ Linux/.xmonad/set_keyboard.sh
@@ -3,7 +3,7 @@
 pcnote=$(xinput list --id-only 'AT Translated Set 2 keyboard')
 
 if [ $? -eq 0 ]; then
-    setxkbmap -device $pcnote -layout jp -option "ctrl:nocaps"
+    setxkbmap -device $pcnote -layout us -option "ctrl:nocaps"
 fi
 
 apple=$(xinput list --id-only 'Apple Inc. Apple Internal Keyboard / Trackpad')
--- Linux/.xmonad/set_monitor.sh
+++ Linux/.xmonad/set_monitor.sh
@@ -5,11 +5,11 @@ if [ -z "$(which xrandr)" ]; then
     exit 1
 fi
 
-INTERNAL="eDP1"
-IS_CONNECT_EXTERNAL="false"
-EXTERNAL_PRIMARY="HDMI1"
-
-xrandr --output "$INTERNAL" --primary --auto
+INTERNAL="eDP-1"
+IS_CONNECT_EXTERNAL_PRIMARY="false"
+IS_CONNECT_EXTERNAL_SECONDARY="false"
+EXTERNAL_PRIMARY="DP-3-8"
+EXTERNAL_SECONDARY="DP-3-1-8"
 
 old_IFS="${IFS}"
 IFS=$'\n'
@@ -19,10 +19,14 @@ for out in `xrandr | grep connected | cut -d ' ' -f 1-2`; do
     case "$state" in
         "connected"*)
             if [ "$name" = "${EXTERNAL_PRIMARY}" ]; then
-                IS_CONNECT_EXTERNAL="true"
-                xrandr --output "$EXTERNAL_PRIMARY" --primary --auto
-            elif [ "$name" != "${INTERNAL}" ]; then
-                xrandr --output "$name" --auto --right-of "${EXTERNAL_PRIMARY}"
+                IS_CONNECT_EXTERNAL_PRIMARY="true"
+            elif [ "$name" = "${EXTERNAL_SECONDARY}" ]; then
+                IS_CONNECT_EXTERNAL_SECONDARY="true"
+            elif [ "$name" = "$INTERNAL" ]; then
+                # do nothing
+                echo ""
+            else
+                xrandr --output "$name" --off
             fi
             ;;
         "disconnected"*)
@@ -34,19 +38,27 @@ for out in `xrandr | grep connected | cut -d ' ' -f 1-2`; do
 done
 IFS="${old_IFS}"
 
-if [ "$IS_CONNECT_EXTERNAL" = "false" ]; then
-    if [ `pidof redshift | wc -w` = 0 ]; then
-        redshift -P -m randr -t 5700:3600 -b 1.0:1.0 -l 35.465:139.622 &
-    fi
-else
-    for pid in `pidof redshift`; do
-        redshift -xP
-        kill $pid
-    done
-    lid_state=`cat /proc/acpi/button/lid/LID0/state | sed 's/ \+/ /g' | cut -d' ' -f2`
-    if [ $lid_state = "closed" ]; then
-        xrandr --output "$INTERNAL" --off
+# output setting for external monitor
+if [ "$IS_CONNECT_EXTERNAL_SECONDARY" = "true" ]; then
+    if [ "$IS_CONNECT_EXTERNAL_PRIMARY" = "true" ]; then
+            xrandr --output "$EXTERNAL_PRIMARY" --primary --auto
+            xrandr --output "$EXTERNAL_SECONDARY" --auto --right-of "$EXTERNAL_PRIMARY"
     else
-        xrandr --output "$INTERNAL" --auto --pos 320x1440
+            xrandr --output "$EXTERNAL_SECONDARY" --primary --auto
+    fi
+elif [ "$IS_CONNECT_EXTERNAL_PRIMARY" = "true" ]; then
+    xrandr --output "$EXTERNAL_PRIMARY" --primary --auto
+fi
+
+# output setting for internal monitor
+if [ `cat /proc/acpi/button/lid/LID/state | cut -d ' ' -f 7` == "closed" ]; then
+    xrandr --output "$INTERNAL" --off
+else
+    if [ "$IS_CONNECT_EXTERNAL_PRIMARY" = "true" ]; then
+        xrandr --output "$INTERNAL" --primary --auto --below "$EXTERNAL_PRIMARY"
+    elif [ "$IS_CONNECT_EXTERNAL_SECONDARY" = "true" ]; then
+        xrandr --output "$INTERNAL" --primary --auto --below "$EXTERNAL_SECONDARY"
+    else
+        xrandr --output "$INTERNAL" --primary --auto
     fi
 fi
