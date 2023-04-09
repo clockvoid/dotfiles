641cf397016e79efd9ae34622bdf033364f89c04
clockvoid <hidenari.t1997nov@gmail.com>
2023-04-10 02:19:15 +0900

    add: keymap selection algorithm on xmonad setting

--- Linux/.config/xmonad/set_keyboard.sh
+++ Linux/.config/xmonad/set_keyboard.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 pcnote=$(xinput list --id-only 'AT Translated Set 2 keyboard')
+keymap=$(localectl status | grep Keymap | cut -d ' ' -f 7)
 
 if [ $? -eq 0 ]; then
-    setxkbmap -device $pcnote -layout us -option "ctrl:nocaps"
+    if [ $keymap = jp106 ]; then
+        setxkbmap -device $pcnote -layout jp -option "ctrl:nocaps"
+    else
+        setxkbmap -device $pcnote -layout us -option "ctrl:nocaps"
+    fi
 fi
 
 apple=$(xinput list --id-only 'Apple Inc. Apple Internal Keyboard / Trackpad')
