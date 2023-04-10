55360df997681f732ff2f41f0effc57b4765712f
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2023-04-11 00:33:30 +0900

    fix: fzf zsh setting file format

--- Linux/.config/zsh/env/fzf.zsh
+++ Linux/.config/zsh/env/fzf.zsh
@@ -1,11 +1,12 @@
-export FZF_DEFAULT_COMMAND='find \( -type d \( \
-           -name .git \
-        \) -prune \) -o -type f -printf "%P\n"'
+export FZF_DEFAULT_COMMAND='find \( \
+    -type d \( \
+    -name .git \
+    \) -prune \) -o -type f -printf "%P\n"'
 
 export FZF_CTRL_T_COMMAND='find \( \
-        -type d \( \
-            -name .git \
-        \) -prune \) -o \( ! -path ./. -type d \) -o -printf "%P\n"'
+    -type d \( \
+    -name .git \
+    \) -prune \) -o \( ! -path ./. -type d \) -o -printf "%P\n"'
 
 # Use fd (https://github.com/sharkdp/fd) instead of the default find
 # command for listing path candidates.
@@ -13,15 +14,15 @@ export FZF_CTRL_T_COMMAND='find \( \
 # - See the source code (completion.{bash,zsh}) for the details.
 _fzf_compgen_path() {
     find "$1" \( -type d \( \
-           -name .git \
+        -name .git \
         \) -prune \) -o -type f -printf "%P\n"
-}
+    }
 
 # Use fd to generate the list for directory completion
 _fzf_compgen_dir() {
-        find "$1" \( \
-            -type d \( \
-                -name .git \
-            \) -prune \
+    find "$1" \( \
+        -type d \( \
+        -name .git \
+        \) -prune \
         \) -o \( ! -path . -type d \) -printf "%P\n"
-}
+    }
