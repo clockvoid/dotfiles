f7ed02296873a11577b3e1e95ef6fe29b1f592b6
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2023-07-25 02:16:12 +0900

    fix: keychain setting to skip to input ssh passphrase

--- Linux/.config/zsh/env/keychain.zsh
+++ Linux/.config/zsh/env/keychain.zsh
@@ -1,7 +1,5 @@
 if ! type "keychain" > /dev/null; then
     echo 'Please isntall keychain using package manager!'
 else
-    if ! { [ -n "$TMUX" ]; } then
-        eval $(keychain --eval --agents ssh -Q --quiet ~/.ssh/id_*)
-    fi
+    eval $(keychain --eval --agents ssh -Q --quiet ~/.ssh/id_*)
 fi
