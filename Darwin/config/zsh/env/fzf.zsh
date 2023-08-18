7b9b31ba348e90597c45d1db7bc8d3c8efc6f4da
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2023-08-18 13:29:22 +0900

    add: loading fzf keybinding for mac

--- Darwin/config/zsh/env/fzf.zsh
+++ Darwin/config/zsh/env/fzf.zsh
@@ -77,5 +77,9 @@ else
     # Auto-completion
     # ---------------
     [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 fi
 
