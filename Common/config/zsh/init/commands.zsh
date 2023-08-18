daa552ea3bf2d222d2823c9f955e4483500f61b5
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2023-08-18 16:40:44 +0900

    fix: integrate zsh scripts

---
+++ Common/config/zsh/init/commands.zsh
@@ -0,0 +1,54 @@
+# fbr - checkout git branch
+fbr() {
+    local branches branch
+    branches=$(git branch --all | grep -v HEAD) &&
+    branch=$(echo "$branches" | fzf --reverse --no-preview -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+    git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remote/[^/]*/##")
+}
+
+# fd - cd to selected directory
+fdr() {
+  local dir prevcmd
+  if ! type tree > /dev/null; then
+      prevcmd='echo "To see perfect preview, install tree" && ls {}'
+  else
+      prevcmd='tree -C {} | head -200'
+  fi
+  dir=$(fd --hidden --follow --exclude ".git" --exclude "Library" --max-depth 5 | fzf +m --reverse --preview "$prevcmd") &&
+  cd "$dir"
+}
+
+# fshow - git commit browser
+fshow() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --no-preview --bind=ctrl-s:toggle-sort \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
+}
+
+# jump to git repository root directory
+# from https://bezhermoso.github.io/2018/07/28/jump-back-up-to-git-repo-root-directory/
+gr() {
+    local _root_dir="$(git rev-parse --show-toplevel 2>/dev/null)"
+    if [[ $? -gt 0 ]]; then
+        >&2 echo 'Not a Git repo!'
+        exit 1
+    fi
+
+    local _pwd=$(pwd)
+    if [[ $_pwd=$_root_dir ]]; then
+        _root_dir="$(git -C $(dirname $_pwd) rev-parse --show-toplevel 2>/dev/null)"
+        if [[ $? -gt 0 ]]; then
+            echo "Already at Git repo root."
+            return 0
+        fi
+    fi
+
+    OLDPWD=$_pwd
+    echo "Git repo root: $_root_dir"
+    cd $_root_dir
+}
--- Common/config/zsh/init/fbr.zsh
+++
@@ -1,9 +0,0 @@
-# fbr - checkout git branch
-
-fbr() {
-    local branches branch
-    branches=$(git branch --all | grep -v HEAD) &&
-    branch=$(echo "$branches" | fzf --reverse --no-preview -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-    git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remote/[^/]*/##")
-}
-
