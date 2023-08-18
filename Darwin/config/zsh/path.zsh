283e7c43e823b4808d9a883a9ffd3833b6f0e4a4
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2023-08-18 16:25:42 +0900

    fix: delete javahome from path.zsh for mac

--- Darwin/config/zsh/path.zsh
+++ Darwin/config/zsh/path.zsh
@@ -7,7 +7,7 @@ export FIND_UTILS=$(brew --prefix)/opt/findutils/libexec/gnubin
 export CORE_UTILS=$(brew --prefix)/opt/coreutils/libexec/gnubin
 export GOPATH=$(go env GOPATH)
 
-export PATH=$FIND_UTILS:$CORE_UTILS:$GOROOT/bin:$JAVA_HOME/bin:$_ANDROID_SDK_HOME/platform-tools:$FLUTTER_HOME/bin:$GOPATH/bin:$PATH
+export PATH=$FIND_UTILS:$CORE_UTILS:$GOROOT/bin:$_ANDROID_SDK_HOME/platform-tools:$FLUTTER_HOME/bin:$GOPATH/bin:$PATH
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "$HOME/.local/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/.local/google-cloud-sdk/path.zsh.inc"; fi
