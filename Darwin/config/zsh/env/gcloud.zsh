393ed3510b800183186553f031ab771c93474056
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2023-08-18 13:10:58 +0900

    fix: move gcloud environment settings for mac to path.zsh

--- Darwin/config/zsh/env/gcloud.zsh
+++
@@ -1,6 +0,0 @@
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f "$HOME/.local/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/.local/google-cloud-sdk/path.zsh.inc"; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f "$HOME/.local/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/.local/google-cloud-sdk/completion.zsh.inc"; fi
-
--- Darwin/config/zsh/path.zsh
+++ Darwin/config/zsh/path.zsh
@@ -9,3 +9,9 @@ export CORE_UTILS=$(brew --prefix)/opt/coreutils/libexec/gnubin
 export GOPATH=$(go env GOPATH)
 
 export PATH=$FIND_UTILS:$CORE_UTILS:$GOROOT/bin:$JAVA_HOME/bin:$_ANDROID_SDK_HOME/platform-tools:$FLUTTER_HOME/bin:$GOPATH/bin:$PATH
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "$HOME/.local/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/.local/google-cloud-sdk/path.zsh.inc"; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f "$HOME/.local/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/.local/google-cloud-sdk/completion.zsh.inc"; fi
