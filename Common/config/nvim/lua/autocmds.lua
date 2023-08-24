07fb7ccefc9de49b16e09245e2aeb257fea441c8
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2023-08-24 21:43:54 +0900

    fix: enable fold with syntax on json files

--- Common/config/nvim/lua/autocmds.lua
+++ Common/config/nvim/lua/autocmds.lua
@@ -1,9 +1,14 @@
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-    pattern = '*.Xresources.d/*',
+    pattern = '*Xresources.d/*',
     command = 'setlocal filetype=xdefaults'
 })
 
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
     pattern = 'zprofile',
     command = 'setlocal filetype=zsh'
+})
+
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+    pattern = '*.json',
+    command = 'setlocal foldmethod=syntax'
 })
