bb65aa1890dd21d3d6819e3cd92ce921d7327cb6
clockvoid <hidenari.t1997nov@gmail.com>
2023-10-01 18:36:07 +0900

    fix: script for compile autohotkey

--- Windows/compile-ahk.bat
+++ Windows/compile-ahk.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
-start %0 "C:\Users\hiden\AppData\Local\Programs\AutoHotkey\Compiler\Ahk2Exe.exe" /in mac.ahk /out mac.exe /base C:\Users\hiden\AppData\Local\Programs\AutoHotkey\v2\AutoHotkey64.exe
+start %0 "%UserProfile%\AppData\Local\Programs\AutoHotkey\Compiler\Ahk2Exe.exe" /in mac.ahk /out mac.exe /base %UserProfile%\AppData\Local\Programs\AutoHotkey\v2\AutoHotkey64.exe
 
