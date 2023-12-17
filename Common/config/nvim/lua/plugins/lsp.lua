44875909762607aa0360e07c4ccb116837507779
clockvoid <hidenari.t1997nov@gmail.com>
2023-12-17 16:08:15 +0900

    fix: code format of lsp.lua

--- Common/config/nvim/lua/plugins/lsp.lua
+++ Common/config/nvim/lua/plugins/lsp.lua
@@ -121,13 +121,13 @@ return {
     }
   },
 
-  { 'hrsh7th/cmp-nvim-lsp',        dependencies = { 'hrsh7th/nvim-cmp', } },
-  { 'hrsh7th/cmp-buffer',          dependencies = { 'hrsh7th/nvim-cmp', } },
-  { 'hrsh7th/cmp-path',            dependencies = { 'hrsh7th/nvim-cmp', } },
-  { 'hrsh7th/cmp-cmdline',         dependencies = { 'hrsh7th/nvim-cmp', } },
-  { 'hrsh7th/cmp-vsnip',           dependencies = { 'hrsh7th/vim-vsnip', 'hrsh7th/nvim-cmp', } },
-  { 'petertriho/cmp-git',          dependencies = { 'hrsh7th/nvim-cmp', } },
-  { 'hrsh7th/nvim-cmp',            config = hook_cmp_loaded, },
+  { 'hrsh7th/cmp-nvim-lsp', dependencies = { 'hrsh7th/nvim-cmp', } },
+  { 'hrsh7th/cmp-buffer',   dependencies = { 'hrsh7th/nvim-cmp', } },
+  { 'hrsh7th/cmp-path',     dependencies = { 'hrsh7th/nvim-cmp', } },
+  { 'hrsh7th/cmp-cmdline',  dependencies = { 'hrsh7th/nvim-cmp', } },
+  { 'hrsh7th/cmp-vsnip',    dependencies = { 'hrsh7th/vim-vsnip', 'hrsh7th/nvim-cmp', } },
+  { 'petertriho/cmp-git',   dependencies = { 'hrsh7th/nvim-cmp', } },
+  { 'hrsh7th/nvim-cmp',     config = hook_cmp_loaded, },
   {
     'hrsh7th/vim-vsnip',
     config = function()
