7672e5ff79bd20d99eabdcd1f05aa8527e06d8e8
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2023-12-28 01:31:33 +0900

    add: install copilot plugin for neovim

--- Common/config/nvim/lua/plugins/lsp.lua
+++ Common/config/nvim/lua/plugins/lsp.lua
@@ -159,4 +159,15 @@ smap <expr> <C-S-k> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-[rev)' : '<C-S-k>'
       }
     end,
   },
+
+  {
+    'github/copilot.vim',
+    config = function()
+      vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false
+      })
+      vim.g.copilot_no_tab_map = true
+    end,
+  }
 }
