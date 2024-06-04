6703dea078acd8c5b3c48399c3bf5ea731f9307f
clockvoid <hidenari.t1997nov@gmail.com>
2024-06-05 00:10:09 +0900

    fix: organize installer script for windows

--- Windows/install-terminal-settings.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-
-xcopy settings.json %UserProfile%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
--- install.bat
+++ install.bat
@@ -1,2 +1,9 @@
 @ECHO OFF
-Windows\install-terminal-settings.bat
+
+xcopy .\Windows\settings.json %UserProfile%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+
+xcopy .\Common\ideavimrc %UserProfile%\.ideavimrc
+
+rmdir /S %UserProfile%\AppData\Local\nvim
+xcopy /e /v .\Common\config\nvim %UserProfile%\AppData\Local\nvim
+xcopy /e /v .\Windows\AppData\Local\nvim\lua\env %UserProfile%\AppData\Local\nvim\lua\env
