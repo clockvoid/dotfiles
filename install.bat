e9b13815c64e841f8e53a66822c29176a67d2aa4
clockvoid <hidenari.t1997nov@gmail.com>
2024-06-05 01:19:33 +0900

    fix: installer script for windows

--- install.bat
+++ install.bat
@@ -5,5 +5,16 @@ xcopy .\Windows\settings.json %UserProfile%\AppData\Local\Packages\Microsoft.Win
 xcopy .\Common\ideavimrc %UserProfile%\.ideavimrc
 
 rmdir /S %UserProfile%\AppData\Local\nvim
-xcopy /e /v .\Common\config\nvim %UserProfile%\AppData\Local\nvim
-xcopy /e /v .\Windows\AppData\Local\nvim\lua\env %UserProfile%\AppData\Local\nvim\lua\env
+if not exist "%UserProfile%\AppData\Local\nvim" (
+  mkdir %UserProfile%\AppData\Local\nvim
+  xcopy /e .\Common\config\nvim %UserProfile%\AppData\Local\nvim
+  mkdir %UserProfile%\AppData\Local\nvim\lua\env
+  xcopy /e .\Windows\AppData\Local\nvim\lua\env %UserProfile%\AppData\Local\nvim\lua\env
+)
+
+rmdir /S %APPDATA%\alacritty
+if not exist "%APPDATA%\alacritty" (
+  mkdir %APPDATA%\alacritty
+  xcopy /e .\Common\config\alacritty %APPDATA%\alacritty
+  xcopy .\Windows\AppData\Roaming\alacritty\env.toml %APPDATA%\alacritty
+)
