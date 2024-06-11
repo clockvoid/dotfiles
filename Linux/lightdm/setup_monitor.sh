74cd62ab711a7c9e0783c164e8d2963c4d55ecdc
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2024-06-12 01:07:35 +0900

    fix: disable user icon on lightdm

--- Linux/lightdm/setup_monitor.sh
+++ Linux/lightdm/setup_monitor.sh
@@ -5,47 +5,60 @@ if [ -z "$(which xrandr)" ]; then
     exit 1
 fi
 
-DISABLE_SUBSTRING="eDP1"
-
-TO_DISABLE=""
-PRIMARY_OUT=""
+INTERNAL="eDP-1"
+IS_CONNECT_EXTERNAL_PRIMARY="false"
+IS_CONNECT_EXTERNAL_SECONDARY="false"
+EXTERNAL_PRIMARY="DP-3-8"
+EXTERNAL_SECONDARY="DP-3-1-8"
 
 old_IFS="${IFS}"
 IFS=$'\n'
 for out in `xrandr | grep connected | cut -d ' ' -f 1-2`; do
     name=`echo $out | cut -d ' ' -f 1`
     state=`echo $out | cut -d ' ' -f 2`
-    if [ "$state" = "connected" ]; then
-        case "$name" in
-            # disable a monitor connected via LVDS
-            ${DISABLE_SUBSTRING}*)
-                if [ -z "$TO_DISABLE" ]; then
-                    TO_DISABLE="$name"
-                fi
-                ;;
-            *)
-                # set the 'first' external monitor as the
-                # primary one, disable the other external monitors
-                if [ -z "$PRIMARY_OUT" ]; then
-                    PRIMARY_OUT="$name"
-                else
-                    xrandr --output "$name" --off
-                fi
-                ;;
-        esac
-    fi
+    case "$state" in
+        "connected"*)
+            if [ "$name" = "${EXTERNAL_PRIMARY}" ]; then
+                IS_CONNECT_EXTERNAL_PRIMARY="true"
+            elif [ "$name" = "${EXTERNAL_SECONDARY}" ]; then
+                IS_CONNECT_EXTERNAL_SECONDARY="true"
+            elif [ "$name" = "$INTERNAL" ]; then
+                # do nothing
+                echo ""
+            else
+                xrandr --output "$name" --off
+            fi
+            ;;
+        "disconnected"*)
+                xrandr --output "$name" --off
+            ;;
+        *)
+            ;;
+    esac
 done
 IFS="${old_IFS}"
 
-# If there is an internal screen, as well as at least one external monitor, then
-# disable the internal monitor and use the external monitor as the primary one.
-#
-# Otherwise, if there are any external monitors, use the 'first' detected one
-# as the primary monitor.
-if [ -n "$TO_DISABLE" ] && [ -n "$PRIMARY_OUT" ]; then
-    xrandr --output "$PRIMARY_OUT" --primary --auto --output "$TO_DISABLE" --off
-elif [ -n "$PRIMARY_OUT" ]; then
-    xrandr --output "$PRIMARY_OUT" --primary --auto
+# output setting for external monitor
+if [ "$IS_CONNECT_EXTERNAL_SECONDARY" = "true" ]; then
+    if [ "$IS_CONNECT_EXTERNAL_PRIMARY" = "true" ]; then
+            xrandr --output "$EXTERNAL_PRIMARY" --primary --auto
+            xrandr --output "$EXTERNAL_SECONDARY" --auto --right-of "$EXTERNAL_PRIMARY"
+    else
+            xrandr --output "$EXTERNAL_SECONDARY" --primary --auto
+    fi
+elif [ "$IS_CONNECT_EXTERNAL_PRIMARY" = "true" ]; then
+    xrandr --output "$EXTERNAL_PRIMARY" --primary --auto
 fi
 
-exit 0
+# output setting for internal monitor
+if [ `cat /proc/acpi/button/lid/LID/state | cut -d ' ' -f 7` == "closed" ]; then
+    xrandr --output "$INTERNAL" --off
+else
+    if [ "$IS_CONNECT_EXTERNAL_PRIMARY" = "true" ]; then
+        xrandr --output "$INTERNAL" --primary --auto --below "$EXTERNAL_PRIMARY"
+    elif [ "$IS_CONNECT_EXTERNAL_SECONDARY" = "true" ]; then
+        xrandr --output "$INTERNAL" --primary --auto --below "$EXTERNAL_SECONDARY"
+    else
+        xrandr --output "$INTERNAL" --primary --auto
+    fi
+fi
