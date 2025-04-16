8e61188dacc52bd197452b395df53d60851e078c
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2025-04-17 02:15:58 +0900

    fix: use lualine and tomorrow-night as colorscheme

--- Common/config/nvim/lua/plugins/colorscheme.lua
+++ Common/config/nvim/lua/plugins/colorscheme.lua
@@ -1,43 +1,29 @@
-local function hook_add_lightline()
-  vim.g.lightline = {
-    colorscheme = 'hybrid',
-    separator = {
-      left = '',
-      right = ''
-    },
-    subseparator = {
-      left = '',
-      right = ''
+local function hook_add_hybrid()
+  vim.cmd.colorscheme('base16-tomorrow-night')
+  vim.opt.termguicolors = true
+  vim.opt.background = 'dark'
+  vim.opt.cursorline = true
+  vim.cmd.highlight('LineNr', 'ctermfg=gray', 'guifg=#707880')
+  vim.cmd.highlight('CursorLineNr', 'ctermfg=white', 'guifg=#c5c8c6')
+  vim.cmd.highlight('MatchParen', 'ctermbg=237', 'ctermfg=255', 'guibg=#3a3a3a', 'guifg=#eeeeee')
+end
+
+local function hook_add_lualine()
+  require('lualine').setup {
+    options = {
+      theme = 'base16',
+      icons_enabled = false,
     },
   }
 end
 
-local function hook_add_hybrid()
-  vim.g.hybrid_custom_term_colors = 0
-  vim.opt.termguicolors = true
-  vim.opt.background = 'dark'
-  vim.cmd.colorscheme('hybrid')
-  vim.opt.cursorline = true
-  vim.cmd.highlight('LineNr', 'ctermfg=lightgray', 'guifg=#707880')
-  vim.cmd.highlight('CursorLine', 'cterm=none', 'ctermfg=none', 'ctermbg=none', 'guifg=none', 'guibg=none')
-  vim.cmd.highlight('CursorLineNr', 'ctermfg=white', 'guifg=#c5c8c6')
-  vim.cmd.highlight('MatchParen', 'ctermbg=237', 'ctermfg=255', 'guibg=#3a3a3a', 'guifg=#eeeeee')
-  vim.cmd.highlight('NormalFloat', 'guibg=#1d1f21')
-  vim.cmd.highlight('FloatBorder', 'guibg=#1d1f21')
-end
-
 return {
   {
-    'w0ng/vim-hybrid',
+    'RRethy/nvim-base16',
     config = hook_add_hybrid
   },
-  { 'cocopon/lightline-hybrid.vim' },
   {
-    'itchyny/lightline.vim',
-    config = hook_add_lightline,
-    dependencies = {
-      'lightline-hybrid.vim',
-      'w0ng/vim-hybrid'
-    },
+    'nvim-lualine/lualine.nvim',
+    config = hook_add_lualine,
   }
 }
