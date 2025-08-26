f6ed0f5d6acc2bc6fb333d717f8c96c09ce27bd2
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2025-08-27 03:03:54 +0900

    fix: color for sign column of cursor line

--- Common/config/nvim/lua/plugins/colorscheme.lua
+++ Common/config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
-local function hook_add_hybrid()
+local function hook_add_base16()
   vim.cmd.colorscheme('base16-tomorrow-night')
   vim.opt.termguicolors = true
   vim.opt.background = 'dark'
   vim.opt.cursorline = true
   vim.cmd.highlight('LineNr', 'ctermfg=gray', 'guifg=#707880')
   vim.cmd.highlight('CursorLineNr', 'ctermfg=white', 'guifg=#c5c8c6')
+  vim.cmd.highlight('CursorLineSign', 'guibg=#282a2e')
   vim.cmd.highlight('MatchParen', 'ctermbg=237', 'ctermfg=255', 'guibg=#3a3a3a', 'guifg=#eeeeee')
 end
 
@@ -20,7 +21,7 @@ end
 return {
   {
     'RRethy/nvim-base16',
-    config = hook_add_hybrid
+    config = hook_add_base16
   },
   {
     'nvim-lualine/lualine.nvim',
