6c549d52f7ca84d7bd839f53129c595b75128e51
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2025-08-27 03:03:16 +0900

    fix: order to load programs when xmonad launches

--- Linux/config/xmonad/xmonad.hs
+++ Linux/config/xmonad/xmonad.hs
@@ -107,14 +107,14 @@ searchList =
 startup :: X ()
 startup = do
   setWMName "LG3D"
+  spawnOnce "picom"
+  spawnOnce "xremap ~/.config/xremap/config.yml --watch"
   spawnOnce "hook_window_focus.sh"
   spawnOnce "nm-applet"
   spawnOnce "blueman-applet"
   spawnOnce "start-pulseaudio-x11"
   spawnOnce "fcitx5"
-  spawnOnce "picom"
   spawnOnce "lxqt-policykit-agent"
-  spawnOnce "xremap ~/.config/xremap/config.yml --watch"
   spawnOnce "volctl"
   spawnOnce
     "trayer --edge top --align right \
