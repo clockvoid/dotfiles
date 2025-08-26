b08681310614d83c516727df378590e276bbf632
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2025-08-27 03:30:40 +0900

    fix: enable spell checking on markdown and tex and gitcommit

--- Common/config/nvim/lua/autocmds.lua
+++ Common/config/nvim/lua/autocmds.lua
@@ -7,3 +7,8 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = 'zprofile',
   command = 'setlocal filetype=zsh'
 })
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'Filetype' }, {
+  pattern = { 'markdown', 'tex', 'gitcommit' },
+  command = 'setlocal spell spelllang=en_us'
+})
