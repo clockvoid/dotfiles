868588c70245aa04928128b38e3a48a9c172e816
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2026-08-23 23:38:19 +0900

    fix: disable GitHub Copilot on Neovim

--- Common/config/nvim/lua/plugins/lsp.lua
+++ Common/config/nvim/lua/plugins/lsp.lua
@@ -184,16 +184,4 @@ return {
       })
     end,
   },
-
-  {
-    'github/copilot.vim',
-    config = function()
-      vim.keymap.set('i', '<Tab>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false
-      })
-      vim.g.copilot_no_tab_map = true
-      vim.g.copilot_filetypes = { markdown = true }
-    end,
-  }
 }
