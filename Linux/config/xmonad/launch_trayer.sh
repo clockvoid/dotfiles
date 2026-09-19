e309768a9b4e5a8a813000a3a47708755db307a7
Hidenari Tajima <hidenari.t1997nov@gmail.com>
2026-09-19 13:44:39 +0900

    fix: make xmobar and trayer height same

--- Linux/config/xmonad/launch_trayer.sh
+++ Linux/config/xmonad/launch_trayer.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
 trayer --edge top --align right \
-    \--widthtype percent --heighttype pixel \
+    \--widthtype pixel --heighttype pixel \
     \--SetDockType true --SetPartialStrut false \
     \--expand false \
-    \--width 8 --height 23 \
+    \--width 160 --height 22 \
     \--transparent true --alpha 0 --tint 0x000000 \
     \--monitor primary
